ef2cf2a168db09848306f95644cb5d89665e602c
hamidskull <hamid.reza.tahmasebi.moradi@gmail.com>
2021-09-06 15:56:53 +0430

    Inventory Domain,ApplicationContracs,Application,Repository Interface implemented

---
+++ Lampshade/InventoryManagement.Application.Contracts/Inventory/CreateInventory.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryManagement.Application.Contracts.Inventory
+{
+    public class CreateInventory
+    {
+        public long ProdcutId { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
---
+++ Lampshade/InventoryManagement.Application.Contracts/Inventory/InventoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryManagement.Application.Contracts.Inventory
+{
+    public class InventoryViewModel
+    {
+        public long Id { get; set; }
+        public string Prodcut { get; set; }
+        public long ProdcutId { get; set; }
+        public double UnitPrice { get; set; }
+        public bool InStock { get; set; }
+        public long CurrentCount { get; set; }
+    }
+}
